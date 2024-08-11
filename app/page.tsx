d6f75a7280463aa6072513c213840e31db3370cb
Trishane <trishaneww@gmail.com>
2024-08-11 12:23:46 -0400

    uploaded file structure

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+const Home = () => {
+  // const isAdmin = searchParams?.admin === "true";
+
+  return (
+    <div className="flex flex-col bg-slate-50 scroll-smooth">
+      <p>Figment Studios</p>
+
+    </div>
+  );
+};
+
+export default Home;
