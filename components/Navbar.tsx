f281291d3d849b71bfb2341df0c922b872b976da
Trishane <trishaneww@gmail.com>
2024-08-12 13:12:38 -0400

    logo carousels

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     <div className="flex gap-6">
       <Link href='/' className={cn(
             "relative dark:text-slate-50 items-center flex space-x-1 text-slate-800 dark:hover:text-slate-300 hover:text-slate-500"
-          )}><span className=" text-sm !cursor-pointer font-bold">Figment Studios</span>
+          )}><span className=" text-xl !cursor-pointer font-bold">Figment Studios</span>
       </Link>
 
       {/* <Link href='/'><span className="text-l font-bold">Argus Doctors</span>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,7 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -67,8 +68,11 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        
       },
       animation: {
+        scroll:
+        "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
